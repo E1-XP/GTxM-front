1a4acda3ebc6ad0dce985fd97d0189c4a199b97f
E1-XP <poczta.elektroniczna@hotmail.com>
2018-09-29 11:31:45 +0200

    init version 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,44 @@
-const path = require('path');
-const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/js/main.js',
-    output: {
-        path: path.resolve(__dirname, "./dist/js"),
-        filename: "bundle.js",
-        publicPath: "/dist/js"
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, "dist")
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env']
-                }
-            }
-        }]
-    }
-    //,
-    // plugins: [
-    //     new uglifyJsPlugin()
-    // ]
+  entry: "./src/index.ts",
+  output: {
+    path: __dirname + "/public",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: ["awesome-typescript-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.hbs$/,
+        use: ["handlebars-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".ts"]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ],
+  devServer: {
+    contentBase: "./public"
+  }
 };
