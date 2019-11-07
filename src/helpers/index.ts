59bb2cf3ee595da5783fd73c64115b8a0b994f06
E!-XP <poczta.elektroniczna@hotmail.com>
2019-11-07 11:47:19 +0100

    fix images path, clean footer by moving some fns to helpers

---
+++ src/helpers/index.ts
@@ -0,0 +1,31 @@
+export const scrollTo = (element: Element, top: number, duration: number) => {
+  const start = element.scrollTop;
+  const difference = top - start;
+  let lastTStamp: number | null = null;
+
+  const moveScroll = (timeLeft: number = 0) => {
+    const calcPos = easeInOutQuad(timeLeft, start, difference, duration);
+    element.scrollTop = calcPos;
+
+    timeLeft < duration &&
+      requestAnimationFrame(tstamp => {
+        const updTimeLeft = lastTStamp
+          ? timeLeft + (tstamp - lastTStamp)
+          : timeLeft;
+
+        lastTStamp = tstamp;
+
+        moveScroll(updTimeLeft);
+      });
+  };
+  moveScroll();
+};
+
+type n = number;
+// by R. Penner http://gizma.com/easing/
+const easeInOutQuad = function(t: n, b: n, c: n, d: n) {
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t + b;
+  t--;
+  return (-c / 2) * (t * (t - 2) - 1) + b;
+};
