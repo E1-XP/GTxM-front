cdadbd3105fdc9de8dff7de8c63ec136541faf58
E!-XP <poczta.elektroniczna@hotmail.com>
2019-11-15 16:56:51 +0100

    signal api connection during loading period

--- src/components/preloader/template.ts
+++ src/components/preloader/template.ts
@@ -2,11 +2,22 @@ import { html } from "project-f";
 
 import { Props } from "./index";
 
-export default ({ value }: Props) =>
-  html`
-  <div class="page_preloader">
+export default ({ value }: Props) => {
+  if (value === "00") {
+    return html`
+      <div class="page_preloader">
+        <span class="page_preloader__content">
+          Connecting...
+        </span>
+      </div>
+    `;
+  }
+
+  return html`
+    <div class="page_preloader">
       <span class="page_preloader__counter">
-            ${value}
+        ${value}
       </span>
-  </div>
-`;
+    </div>
+  `;
+};
