3f2cce924e79733b3a16e9719d642919efff6fcb
E1-XP <poczta.elektroniczna@hotmail.com>
2022-04-24 20:10:50 +0200

    move api url and use netlify proxy

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
 export default {
-  API_URL: `https://boiling-citadel-14104.herokuapp.com`
+  API_URL:
+    process.env.NODE_ENV === "production"
+      ? `${window.location.origin}/api`
+      : "https://gtxmotorsports.gtxcodeworks.site",
+  API_URL_ORIGIN: "https://gtxmotorsports.gtxcodeworks.site",
 };
