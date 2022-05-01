3079ee89b7ee9f76860938a87ab0f0f178e3b09b
E1-XP <poczta.elektroniczna@hotmail.com>
2022-05-01 19:44:56 +0200

    remove file-loader to fix image path

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -42,16 +42,6 @@ module.exports = {
           },
         ],
       },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            outputPath: "/public",
-            publicPath: "/public",
-          },
-        },
-      },
     ],
   },
   resolve: {
