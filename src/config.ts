1cbebc25da24dfc935c0a87c4abd25d5e2e8da4d
E1-XP <poczta.elektroniczna@hotmail.com>
2023-06-06 11:53:01 +0200

    use redirects to fix cors error

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
   API_URL:
     process.env.NODE_ENV === "production"
-      ? "https://gtxmotorsports.gtxcodeworks.online"
+      ? `${window.location.origin}/api`
       : "http://localhost:3001",
   API_URL_ORIGIN: "https://gtxmotorsports.gtxcodeworks.online",
 };
