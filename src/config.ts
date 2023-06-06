b38290ba09acb69ccfe7124f81ce0f81cb01157b
E1-XP <poczta.elektroniczna@hotmail.com>
2023-06-06 17:24:16 +0200

    Revert "use redirects to fix cors error"
    This reverts commit 1cbebc25da24dfc935c0a87c4abd25d5e2e8da4d.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
   API_URL:
     process.env.NODE_ENV === "production"
-      ? `${window.location.origin}/api`
+      ? "https://gtxmotorsports.gtxcodeworks.online"
       : "http://localhost:3001",
   API_URL_ORIGIN: "https://gtxmotorsports.gtxcodeworks.online",
 };
