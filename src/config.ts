66cb627caaa42a773da6eada53c124e468cd4485
E1-XP <sebastian-stypula@hotmail.com>
2023-12-13 17:09:21 +0100

    update api url

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
   API_URL:
     process.env.NODE_ENV === "production"
-      ? "https://gtxmotorsports.gtxcodeworks.online"
+      ? "https://gtxmotorsports.gtxcodeworks.com.pl"
       : "http://localhost:3001",
-  API_URL_ORIGIN: "https://gtxmotorsports.gtxcodeworks.online",
+  API_URL_ORIGIN: "https://gtxmotorsports.gtxcodeworks.com.pl",
 };
